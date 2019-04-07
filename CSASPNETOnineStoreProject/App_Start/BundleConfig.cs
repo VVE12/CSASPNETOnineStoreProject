137ee8b6fb3a7310ee20d5f979760f1e1d6593f1
VVE12 <vasilievvladislav12@gmail.com>
2019-04-07 17:59:07 +0300

    Second commit

--- CSASPNETOnineStoreProject/App_Start/BundleConfig.cs
+++ CSASPNETOnineStoreProject/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace CSASPNETOnineStoreProject
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/jquery-{version}.js", "~/Scripts/bootstrap.js"));
+
             // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
             // готово к выпуску, используйте средство сборки по адресу https://modernizr.com, чтобы выбрать только необходимые тесты.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
